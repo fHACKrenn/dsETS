bc07bc8c4eb9e7b0bd0989443dc5a509a735426b
fHACKrenn <git.fachren.hub@gmail.com>
2024-10-29 20:02:22 +0700

    Update

--- environment/workmesin1/mesin1/client_simple.py
+++ environment/workmesin1/mesin1/client_simple.py
@@ -28,15 +28,13 @@ def send_request(ctx, endpoint, request):
 
 # Setup command-line argument parsing
 parser = argparse.ArgumentParser(description="File Client")
-parser.add_argument('endpoint', help="ZMQ endpoint to connect to (e.g., tcp://localhost:5555)")
-subparsers = parser.add_subparsers(dest="command", help="Sub-command (list or download)")
+parser.add_argument('endpoints', nargs='+', help="ZMQ endpoints to connect to (e.g., tcp://localhost:5555)")
 
 # Sub-command for --list
-parser_list = subparsers.add_parser('list', help="List files on the server")
+parser.add_argument('--list', action='store_true', help="List files on the server")
 
 # Sub-command for --download
-parser_download = subparsers.add_parser('download', help="Download a file from the server")
-parser_download.add_argument('filename', help="The name of the file to download")
+parser.add_argument('--download', type=str, help="Download a file from the server. Specify the filename.")
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -44,26 +42,32 @@ args = parser.parse_args()
 # Create ZMQ context
 context = zmq.Context()
 
-if args.command == "list":
+# Prepare the request based on the command
+if args.list:
     request = ["list".encode("utf-8")]
-elif args.command == "download":
-    request = ["download".encode("utf-8"), args.filename.encode("utf-8")]
+elif args.download:
+    request = ["download".encode("utf-8"), args.download.encode("utf-8")]
 else:
-    print("Invalid command. Usage : python flclient2.py {endpoint} {list,download}")
+    print("Invalid command. Usage: python client_simple.py {endpoints} [--list | --download filename]")
     sys.exit(1)
 
-# Send request to the endpoint and receive the reply
-reply = send_request(context, args.endpoint, request)
+# Iterate through the endpoints
+for endpoint in args.endpoints:
+    reply = send_request(context, endpoint, request)
+    
+    if reply:
+        if reply[1] == ERRN:
+            print(f"Error from {reply[0].decode('utf-8')}: {reply[2].decode('utf-8')}")
+        elif reply[1] == MESG:
+            print(f"Message from {reply[0].decode('utf-8')}:\n{reply[2].decode('utf-8')}")
+        elif reply[1] == FILE:
+            filename = reply[2].decode("utf-8")
+            with open(filename, "wb") as f:
+                f.write(reply[3])
+            print(f"File {filename} downloaded successfully")
+        break  # Stop after a successful request
+    else:
+        print(f"No response from {endpoint}.")
 
-if reply:
-    if reply[1] == ERRN:
-        print(f"Error from {reply[0].decode('utf-8')}: {reply[2].decode('utf-8')}")
-    elif reply[1] == MESG:
-        print(f"Message from {reply[0].decode('utf-8')}:\n{reply[2].decode('utf-8')}")
-    elif reply[1] == FILE:
-        filename = reply[2].decode("utf-8")
-        with open(filename, "wb") as f:
-            f.write(reply[3])
-        print(f"File {filename} downloaded successfully")
-else:
-    print("No response from server.")
+# Clean up context
+context.term()
--- environment/workmesin1/mesin1/server_simple.py
+++ environment/workmesin1/mesin1/server_simple.py
@@ -28,9 +28,7 @@ while True:
         break
 
     command = msg[0].decode("utf-8")
-    filename = None
-    if len(msg) > 1:
-        filename = msg[1].decode("utf-8")
+    filename = msg[1].decode("utf-8") if len(msg) > 1 else None
 
     if command == "list":
         files = [f for f in listdir(FILEPATH) if isfile(join(FILEPATH, f))]
@@ -46,3 +44,6 @@ while True:
         res = [HOSTNAME.encode("utf-8"), ERRN, "ERROR: Invalid command".encode("utf-8")]
 
     server.send_multipart(res)
+
+# Clean up context (this line will not be reached in a while True loop)
+context.term()
